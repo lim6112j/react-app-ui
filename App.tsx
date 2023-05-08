33fd915dcb4b97092ec8e284c282cf27db502a51
lim6112j <lim6112j@gmail.com>
2023-05-08 22:51:14 +0900

    step3

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 import Intro from './Intro';
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
 
   console.log("rendering parent...");
   return (
     <View>
-      <Text>you clicked {count} times. </Text>
+      <Text>you clicked {count} times. typed {text}</Text>
+      <TextInput onChangeText={(d) => setText(d)} />
       <Button title="click" onPress={() => setCount(count + 1)}  />
       <Text>
         Parent View
