1913b9dcbcc9453671d8dab2745dbf895897400d
lim6112j <lim6112j@gmail.com>
2023-05-09 03:09:11 +0900

    prettifier

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, TextInput, View} from 'react-native';
 import Intro from './Intro';
 
 const App = () => {
-  const [count, setCount] = useState(0)
-  const [text, setText] = useState("")
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
 
-  console.log("rendering parent...");
+  console.log('rendering parent...');
   return (
     <View>
-      <Text>you clicked {count} times. typed {text}</Text>
-      <TextInput onChangeText={(d) => setText(d)} />
-      <Button title="click" onPress={() => setCount(count + 1)}  />
       <Text>
-        Parent View
+        you clicked {count} times. typed {text}
       </Text>
+      <TextInput onChangeText={d => setText(d)} />
+      <Button title="click" onPress={() => setCount(count + 1)} />
+      <Text>Parent View</Text>
       <Intro />
     </View>
   );
--- Intro.tsx
+++ Intro.tsx
@@ -1,14 +1,13 @@
-import React, { Component, memo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {memo} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const Intro = (props: any) => {
-  console.log("rendering child");
+  console.log('rendering child');
   return (
     <View>
-      <Text>
-        Child View
-      </Text>
+      <Text>Child View</Text>
     </View>
-  )
-}
+  );
+};
 export default memo(Intro);
